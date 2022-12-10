9fedf08ad15ee9ef32b83656bdf544f099e3d7ba
timedev-net <drfrota.adv@gmail.com>
2022-12-10 12:57:30 -0400

    add prefix name

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.setGlobalPrefix("minhas_contas")
   await app.listen(4000);
 }
 bootstrap();
